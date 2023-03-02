e42bb9977075b76646ddaecd75475fd0ac679f83
f11894 <16046279+f11894@users.noreply.github.com>
2023-03-02 20:51:37 +0900

    v0.6
    xargs_threadsを物理コアと同じ数値に変更
    デフォルトエンコーダーをlibaomに変更
    libaomのエンコード設定を変更
    一時フォルダをbatファイルと同じ場所に変更
    エンコードを中断しても再開出来るようにした
    batを終了したときにxargsで起動したプロセスも閉じるようにした

--- mt_encode.bat
+++ mt_encode.bat
@@ -1,15 +1,15 @@
 @echo off
 rem -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-set xargs_threads=6
+set /a xargs_threads=NUMBER_OF_PROCESSORS/2
 set Scene_change_threshold=0.45
 set min_frame_division_interval=25
 set max_frame_division_interval=250
-set video_encoder=libvpx
+set video_encoder=libaom
 set audio_extension=opus
 set output_extension=mkv
 
-set libaom_option=--cpu-used=4 --row-mt=0 --tile-columns=1 --tile-rows=0 --threads=2 --end-usage=q --cq-level=35 --kf-max-dist=250 --kf-min-dist=250
-set libvpx_option=--cpu-used=1 --row-mt=0 --tile-columns=1 --tile-rows=0 --threads=2 --end-usage=q --cq-level=35 --kf-max-dist=250 --kf-min-dist=250 --auto-alt-ref=6
+set libaom_option=--cpu-used=5 --row-mt=0 --tile-columns=1 --tile-rows=0 --threads=4 --end-usage=q --cq-level=35 --kf-max-dist=250 --kf-min-dist=250
+set libvpx_option=--cpu-used=1 --row-mt=0 --tile-columns=1 --tile-rows=0 --threads=4 --end-usage=q --cq-level=35 --kf-max-dist=250 --kf-min-dist=250 --auto-alt-ref=6
 set libopus_option=-acodec libopus -b:a 128K -frame_duration 60
 set qaac_option=-q 2 --tvbr 82
 
@@ -32,15 +32,11 @@ if not defined sub_bat (
     echo エラー 無効なvideo_encoderです:"%video_encoder%"
     goto error_label
 )
-set "TemporaryDir=%~dp1Temporary\"
+set "TemporaryDir=%~dp0Temporary\"
 for /f "delims=" %%a in ('PowerShell "-Join (Get-Random -Count 32 -input 0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)"') do set "random32=%%a"
 if not exist "%TemporaryDir%" mkdir "%TemporaryDir%"
-set avs_file="%TemporaryDir%\%random32%.avs"
-for %%i  in ("%TemporaryDir%\%random32%.avs") do set "avs_file_name=%%~nxi"
-set audio_file="%TemporaryDir%%random32%_audio.%audio_extension%"
-set xargs_txt="%TemporaryDir%\%random32%_xargs.txt"
-set scenechange_txt="%TemporaryDir%\%random32%_scenechange.txt"
-set concat_txt="%TemporaryDir%\%random32%_concat.txt"
+set avs_file="%TemporaryDir%%random32%.avs"
+for %%i  in ("%TemporaryDir%%random32%.avs") do set "avs_file_name=%%~nxi"
 if /i "%~x1"==".avs" (
     echo 入力したavsをそのまま使用します 
     copy /y "%~1" %avs_file% >nul 2>&1
@@ -55,33 +51,46 @@ if /i "%~x1"==".avs" (
     )
     echo AudioDub^(V, A^)
 )>%avs_file%
+if "%video_encoder%"=="libaom" echo # libaom_option %libaom_option%>>%avs_file%
+if "%video_encoder%"=="libvpx" echo # libvpx_option %libvpx_option%>>%avs_file%
+echo # Scene_change_threshold %Scene_change_threshold%>>%avs_file%
+for /f "delims=" %%i in ('certutil -hashfile %avs_file% SHA1^|find /v ":"') do set "SHA1=%%i"
+
+set audio_file="%TemporaryDir%%SHA1%_audio.%audio_extension%"
+set xargs_txt="%TemporaryDir%%SHA1%_xargs.txt"
+set scenechange_txt="%TemporaryDir%%SHA1%_scenechange.txt"
+set concat_txt="%TemporaryDir%%SHA1%_concat.txt"
+
 for %%i in (%avs2pipemod64%) do pushd "%%~dpi"
 for /f "tokens=2" %%a in ('.\avs2pipemod64.exe -info %avs_file% ^| find "v:frames"') do set "frame_count=%%a"
 %avs2pipemod64% -info %avs_file% 
 popd
-echo 音声のエンコードを開始 
-if /i "%audio_extension%"=="opus" start "" /min %ffmpeg% -y -i %avs_file% -vn %libopus_option% %audio_file%
-if /i "%audio_extension%"=="m4a"  start "" /min %comspec% /c "%ffmpeg% -y -loglevel quiet -i %avs_file% -vn -f wav - | %qaac% %qaac_option% --ignorelength -o %audio_file% "-" "
-if exist "%~dp1keyframelist.txt" (
-    echo keyframelist.txtからキーフレーム情報を取得します 
-    copy /y "%~dp1keyframelist.txt" %scenechange_txt% >nul 2>&1
-) else (
-    pushd "%TemporaryDir%"
-    echo シーンチェンジを検出中 
-    %ffprobe% -select_streams v -show_entries frame=pkt_pts -of compact=p=0:nk=1 -f lavfi "movie=%avs_file_name%,setpts=N+1,select=gt(scene\,%Scene_change_threshold%)">%scenechange_txt% 2>nul
-    popd
+if not exist %audio_file% echo 音声のエンコードを開始 
+if not exist %audio_file% if /i "%audio_extension%"=="opus" start "" /min %ffmpeg% -y -i %avs_file% -vn %libopus_option% %audio_file%
+if not exist %audio_file% if /i "%audio_extension%"=="m4a"  start "" /min %comspec% /c "%ffmpeg% -y -loglevel quiet -i %avs_file% -vn -f wav - | %qaac% %qaac_option% --ignorelength -o %audio_file% "-" "
+if not exist %scenechange_txt% (
+    if exist "%~dp1keyframelist.txt" (
+        echo keyframelist.txtからキーフレーム情報を取得します 
+        copy /y "%~dp1keyframelist.txt" %scenechange_txt% >nul 2>&1
+    ) else (
+        pushd "%TemporaryDir%"
+        echo シーンチェンジを検出中 
+        %ffprobe% -select_streams v -show_entries frame=pkt_pts -of compact=p=0:nk=1 -f lavfi "movie=%avs_file_name%,setpts=N+1,select=gt(scene\,%Scene_change_threshold%)">%scenechange_txt% 2>nul
+        popd
+    )
 )
 set /a frame_count_plus_1=frame_count+1
-echo %frame_count_plus_1% >>%scenechange_txt%
+find "%frame_count_plus_1%" %scenechange_txt% >nul 2>&1 || echo %frame_count_plus_1% >>%scenechange_txt%
 rem デバッグ用 
 rem echo LanczosResize(320,180)>>%avs_file%
 rem echo info()>>%avs_file%
 set start_f=0
 if not defined min_frame_division_interval set min_frame_division_interval=25
 if not defined max_frame_division_interval set max_frame_division_interval=250
-for /f "delims=" %%i in ('Type %scenechange_txt%') do (
+if not exist %xargs_txt% for /f "delims=" %%i in ('Type %scenechange_txt%') do (
     call :division %%i
 )
+if not defined job_num for /f "tokens=3" %%i in ('find /v /c "" %xargs_txt%') do set "job_num=%%i"
 set /a frame_count_minus1=frame_count-1
 find "%frame_count_minus1%" %xargs_txt% >nul 2>&1
 if not "%ERRORLEVEL%"=="0" (
@@ -96,26 +105,27 @@ echo Total number of jobs    : %job_num%
 echo xargs threads           : %xargs_threads%
 echo,
 for %%i in (%timer64%) do pushd "%%~dpi"
-for /f "tokens=3" %%a in ('.\timer64.exe %busybox64% xargs -a %xargs_txt% -n 3 -P %xargs_threads% %comspec% /c start "" /wait /min %sub_bat% ^| find "TotalMilliseconds"') do set "TotalMilliseconds=%%a"
+for /f "tokens=3" %%a in ('.\timer64.exe %busybox64% xargs -a %xargs_txt% -n 3 -P %xargs_threads% %comspec% /c start "" /wait /b %sub_bat% ^| find "TotalMilliseconds"') do set "TotalMilliseconds=%%a"
 popd
 for /f "delims=" %%a in ('PowerShell "%TotalMilliseconds%/1000"') do set "TotalSeconds=%%a"
 for /f "delims=" %%a in ('PowerShell "%frame_count%/%TotalSeconds%"') do set "enc_fps=%%a"
-echo Elapsed time(sec)       : %TotalSeconds%
-echo Total encoding fps      : %enc_fps%
-echo,
 pushd "%TemporaryDir%"
-for /L %%i in (1,1,%job_num%) do echo file '%random32%_video%%i.%video_extension%'>>%concat_txt%
+if not exist %%concat_txt%% for /L %%i in (1,1,%job_num%) do echo file '%SHA1%_video%%i.%video_extension%'>>%concat_txt%
 echo 動画と音声を結合します 
 echo,
 %ffmpeg% -y -hide_banner -f concat -safe 0 -i %concat_txt% -i %audio_file% -c copy "%~dpn1_%video_encoder%_%audio_extension%.%output_extension%"
 if "%ERRORLEVEL%"=="0" (
-    for /L %%i in (1,1,%job_num%) do del "%random32%_video%%i.%video_extension%"
+    for /L %%i in (1,1,%job_num%) do del "%SHA1%_video%%i.%video_extension%"
+    for /L %%i in (1,1,%job_num%) do del "%SHA1%_video%%i.txt"
     del %audio_file%
     del %concat_txt%
     del %scenechange_txt%
     del %xargs_txt%
     del %avs_file%
 )
+echo,
+echo Elapsed time(sec)       : %TotalSeconds%
+echo Total encoding fps      : %enc_fps%
 echo,
 popd
 :error_label
--- sub_bat/sub_libaom.bat
+++ sub_bat/sub_libaom.bat
@@ -1,6 +1,8 @@
-title job %3
-%avs2pipemod64% -trim=%1,%2 -y4mp %avs_file% | %aomenc% %libaom_option% --ivf --passes=2 --pass=1 --fpf="%TemporaryDir%%random32%_video%3.fpf" -o "%TemporaryDir%%random32%_video%3.%video_extension%" -
-%avs2pipemod64% -trim=%1,%2 -y4mp %avs_file% | %aomenc% %libaom_option% --ivf --passes=2 --pass=2 --fpf="%TemporaryDir%%random32%_video%3.fpf" -o "%TemporaryDir%%random32%_video%3.%video_extension%" -
-if not "%ERRORLEVEL%"=="0" pause
-del "%TemporaryDir%%random32%_video%3.fpf"
+if exist "%TemporaryDir%%SHA1%_video%3.txt" exit
+%avs2pipemod64% -trim=%1,%2 -y4mp %avs_file% 2>nul | %aomenc% %libaom_option% --ivf --passes=2 --pass=1 --fpf="%TemporaryDir%%SHA1%_video%3.fpf" -o "%TemporaryDir%%SHA1%_video%3.%video_extension%" - >nul 2>&1
+%avs2pipemod64% -trim=%1,%2 -y4mp %avs_file% 2>nul | %aomenc% %libaom_option% --ivf --passes=2 --pass=2 --fpf="%TemporaryDir%%SHA1%_video%3.fpf" -o "%TemporaryDir%%SHA1%_video%3.%video_extension%" - >nul 2>&1
+if not "%ERRORLEVEL%"=="0" echo エンコードに失敗しました
+del "%TemporaryDir%%SHA1%_video%3.fpf"
+echo done>"%TemporaryDir%%SHA1%_video%3.txt"
+title Progress %3/%job_num%
 exit
